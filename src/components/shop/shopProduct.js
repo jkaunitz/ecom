3430ac6de8871ce5d5f3dbe00ce6e910692bffdc
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-09-21 11:44:35 -0700

    mapped cartproducts into cart product component instances

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -4,7 +4,7 @@ import GreenPriceTag from '../greenPriceTag';
 
 class ShopProduct extends Component {
     render() {
-        const { _id, title, description, price } = this.props;
+        const { _id, title, quantity, price } = this.props;
         return (
             <div className='shop-product'>
                 <div className='shop-product__front'>
@@ -19,7 +19,7 @@ class ShopProduct extends Component {
                         {description}
                     </div>
                     <GreenPriceTag className='shop-product__back__price' title={price}/>
-                    <Quantity className='shop-product__back__quantity' quantity={1}/>
+                    <Quantity className='shop-product__back__quantity' quantity={quantity}/>
                     <div className='shop-product__back__add-to-cart'>
                         Add to Cart
                     </div>
